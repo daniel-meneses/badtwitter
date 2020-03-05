96f6bf06eedaea68ebc1f03c336802e6239040f4
daniel-meneses <dmenesesmailbox@gmail.com>
2020-03-05 15:36:38 +1100

    Update post mini props

--- src/components/PostMini/PostMini.tsx
+++ src/components/PostMini/PostMini.tsx
@@ -1,46 +1,51 @@
 import React, {useState} from "react"
 import './PostMini.scss'
-import LikeButton from '../LikeButton/LikeButton'
 import { connect } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import moment from 'moment'
+import LikeButton from '../LikeButton/LikeButton'
 import FloatingImage from '../FloatingImageContainer/FloatingImage';
 
 type Props = {
   postId: number,
-  posts: { [index: string] :
-              { id: number,
-                user_id: number,
-                post: any,
-                created: string }
-              },
-  users: any,
+  post: { id: number,
+          user_id: number,
+          post: any,
+          created: string
+        },
+  user: { alias: string,
+          avatar: string,
+          bio: string,
+          first_name: string,
+          last_name: string,
+          user_id: number
+        }
 }
 
-function mapStateToProps(state :any) {
+function mapStateToProps(state :any, ownProps: any) {
+  const global = state.globalObject
+  const post = global.posts[ownProps.postId]
+  const user = global.users[post.user_id]
   return {
-    posts: state.globalObject.posts,
-    users: state.globalObject.users,
+    post: post,
+    user: user
    };
 }
 
-const PostMiniComponent = ({postId, posts, users}: Props) => {
+const PostMiniComponent = ({postId, post, user}: Props) => {
   const [displayFloatingImage, setDisplayFloatingImage] = useState(false)
   let history = useHistory()
-  let post = posts[postId];
-  if (post === undefined) {
+  if (post === undefined || user===undefined) {
     return <></>
   }
-  let user = users[post.user_id];
   var timeStamp = moment(post.created).format("MMM Do LT");;
 
   return (
-      <div  className='post_mini_component'
-            data-key={post.id}>
+      <div className='post_mini_component' data-key={post.id}>
         <img src={user.avatar} onClick={() => setDisplayFloatingImage(true)}/>
         <h3 data-key={post.user_id}
             onClick={() => history.push("/user/" + user.user_id)}>
-              {user.first_name + " " + user.last_name}
+            {user.first_name + " " + user.last_name}
         </h3>
         <FloatingImage isDisplayed={displayFloatingImage}
                        image={user.avatar}
