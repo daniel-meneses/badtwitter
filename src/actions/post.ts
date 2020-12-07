0131582de24e601df30aeb228bb186d29283a092
daniel-meneses <dmenesesmailbox@gmail.com>
2020-12-07 19:20:14 +1100

    https

--- src/actions/post.ts
+++ src/actions/post.ts
@@ -32,7 +32,7 @@ export function getLinkPreview(linkURL: string): AppThunk {
   let type = PostFormReqTypes.GET_LINK_PREVIEW
   return (dispatch: Dispatch) => {
     dispatch(actions.reqStart(type))
-    let base = 'http://api.linkpreview.net/?key=317510c11e6d6336d33793e4f8f1ed13&q=';
+    let base = 'https://api.linkpreview.net/?key=317510c11e6d6336d33793e4f8f1ed13&q=';
     return fetch(base + linkURL, { method: 'GET'})
     .then( response => {
       return response.json().then((data: any) => {
